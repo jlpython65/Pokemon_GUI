e7539b2d0efec3c65bbdbc87509955076947cd4d
unknown <jasonpythonacc@gmail.com>
2022-07-09 12:16:31 -0400

    Brought back the deleted version

--- #GUI_Pokemon.py
+++ #GUI_Pokemon.py
@@ -1,52 +1,92 @@
-#GUI_Pokemon
-from email.mime import image
-import imp
-import tkinter as tk
-from PIL import ImageTk, Image  
-
-
 #!/usr/bin/python3
 import tkinter as tk
+import tkinter.ttk as ttk
 
 
-class TestApp:
+class StartApp:
     def __init__(self, master=None):
         # build ui
         self.frame2 = tk.Frame(master)
-        self.labelframe1 = tk.LabelFrame(self.frame2)
-        self.pokemon = tk.Label(self.labelframe1)
-        self.img_Milotic = tk.PhotoImage(file=r"C:\Users\JASON LEE\Documents\1Python\Pokemon_Game\Pokemon_Images\gen5\Milotic.png")
+        self.frame1 = ttk.Frame(self.frame2)
+        self.pokemon = tk.Label(self.frame1)
+        self.img_Abra = tk.PhotoImage(file=r"C:\Users\JASON LEE\Documents\1Python\Pokemon_Game\Pokemon_Images\gen5\Absol.png")
         self.pokemon.configure(
-            font="TkDefaultFont", height=196, image=self.img_Milotic, justify="left"
+            anchor="center", bitmap="error", font="TkTextFont", height=196
         )
         self.pokemon.configure(
-            relief="flat", state="normal", takefocus=True, text="label1"
+            image=self.img_Abra, justify="center", relief="raised", state="normal"
         )
-        self.pokemon.configure(width=196)
-        self.pokemon.pack(side="top")
-        self.labelframe1.configure(
-            cursor="arrow", height=196, takefocus=False, text="labelframe1"
+        self.pokemon.configure(takefocus=False, text="label1", width=196)
+        self.pokemon.pack()
+        self.frame1.configure(height=500, width=500)
+        self.frame1.grid(column=0, row=0, sticky="n")
+        self.frame3 = ttk.Frame(self.frame2)
+        self.button1 = tk.Button(self.frame3)
+        self.button1.configure(
+            borderwidth=10, default="normal", font="TkDefaultFont", justify="right"
         )
-        self.labelframe1.configure(width=196)
-        self.labelframe1.pack(side="top")
-        self.frame3 = tk.Frame(self.frame2, container="true")
-        self.frame3.configure(cursor="based_arrow_up", height=200, width=500)
-        self.frame3.pack(side="top")
-        self.button1 = tk.Button(self.frame2)
+        self.button1.configure(state="normal", text="CATCH")
+        self.button1.pack(side="left")
+        self.label3 = ttk.Label(self.frame3)
+        self.img_pokeball1 = tk.PhotoImage(file=r"C:\Users\JASON LEE\Documents\1Python\Pokemon_Game\GUI\pokeball (1).png")
+        self.label3.configure(
+            compound="center",
+            font="{Arial Narrow} 12 {bold}",
+            foreground="#fb5846",
+            image=self.img_pokeball1,
+        )
+        self.label3.pack(expand="true", padx=20, side="top")
+        self.label4 = ttk.Label(self.frame3)
+        self.label4.configure(text="10x")
+        self.label4.pack(side="top")
+        self.frame3.configure(cursor="arrow", height=100, width=200)
+        self.frame3.grid(column=0, row=2)
+        self.frame7 = ttk.Frame(self.frame2)
+        self.frame7.configure(width=300)
+        self.frame7.grid()
+        self.frame2.configure(height=300, relief="flat", takefocus=False, width=300)
+        self.frame2.grid(column=0, row=0)
 
-        catch_icon = tk.PhotoImage(r'C:\Users\JASON LEE\Documents\1Python\Pokemon_Game\GUI\poke_ball.png')
+        # Main widget
+        self.mainwindow = self.frame2
 
-        self.button1.configure(
-            anchor="n", borderwidth=25, compound="top", cursor="arrow"
+    def run(self):
+        self.mainwindow.mainloop()
+
+
+class CatchApp:
+    def __init__(self, master=None):
+        # build ui
+        self.frame2 = tk.Frame(master)
+        self.frame1 = ttk.Frame(self.frame2)
+        self.pokemon = tk.Label(self.frame1)
+        self.img_Absol = tk.PhotoImage(file=r"C:\Users\JASON LEE\Documents\1Python\Pokemon_Game\Pokemon_Images\gen5\Absol.png")
+        self.pokemon.configure(
+            anchor="center", font="TkDefaultFont", height=196, image=self.img_Absol
         )
-        self.button1.configure(
-            default="normal", font="TkTextFont", justify="center", overrelief="sunken"
+        self.pokemon.configure(
+            justify="center", relief="raised", state="normal", takefocus=False
         )
-        self.button1.configure(
-            relief="raised", state="active", takefocus=False, text="CATCH", image= catch_icon
+        self.pokemon.configure(text="label1", width=196)
+        self.pokemon.pack()
+        self.frame1.configure(height=500, takefocus=False, width=500)
+        self.frame1.grid(column=0, row=0, sticky="n")
+        self.frame3 = ttk.Frame(self.frame2)
+        self.label1 = ttk.Label(self.frame3)
+        self.label1.configure(
+            takefocus=False,
+            text="Absol has been caught! Would you like to give a nickname?",
         )
-        self.frame2.configure(height=200, width=200)
-        self.frame2.pack(side="top")
+        self.label1.pack(side="top")
+        self.entry2 = ttk.Entry(self.frame3)
+        self.entry2.pack(side="top")
+        self.frame3.configure(cursor="arrow", height=100, width=200)
+        self.frame3.grid(column=0, row=2)
+        self.frame7 = ttk.Frame(self.frame2)
+        self.frame7.configure(width=300)
+        self.frame7.grid()
+        self.frame2.configure(height=300, relief="flat", takefocus=False, width=300)
+        self.frame2.grid(column=0, row=0)
 
         # Main widget
         self.mainwindow = self.frame2
@@ -57,7 +97,7 @@ class TestApp:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = TestApp(root)
+    app = StartApp(root)
+    app.button1.bind("<Button>", 
+        lambda e: CatchApp(root))
     app.run()
-    print("hello1")
-
